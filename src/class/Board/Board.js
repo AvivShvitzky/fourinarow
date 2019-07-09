f055fea2447ec371f8840d0b6a7c114bf2daa7bf
AvivShvitzky <40824243+AvivShvitzky@users.noreply.github.com>
2019-07-09 15:25:36 +0300

    Update Board.js

--- src/class/Board/Board.js
+++ src/class/Board/Board.js
@@ -107,59 +107,8 @@ class Board {
 
     return true
   }
-  
-    isMatch(row, col) {
-      // code goes here...
-  }
-  
-  isMatchOneDirection(row, col, rowToAdd, colToAdd) {
-    let numOfMarks = 1
-    numOfMarks = this.marksInDirection(row, col, rowToAdd, colToAdd, numOfMarks)
-    if (numOfMarks === WINNING_MATCH) return true
-    numOfMarks = this.marksInDirection(row, col, -rowToAdd, -colToAdd, numOfMarks)
-    if (numOfMarks === WINNING_MATCH) return true
-    return false
-  }
-  marksInDirection(_row, _col, rowToAdd, colToAdd, _numOfMarks) {
-    let numOfMarks = _numOfMarks
-    let row = _row
-    let col = _col
-    while (!this.overEdge(row, col, rowToAdd, colToAdd)) {
-      if (this._board[row + rowToAdd][col + colToAdd] === this._mark) {
-        if (++numOfMarks === WINNING_MATCH) return WINNING_MATCH
-        row += rowToAdd
-        col += colToAdd
-      } else {
-        return numOfMarks
-      }
-    }
-    return numOfMarks
-  }
-  overEdge(row, col, rowToAdd, colToAdd) {
-    // direction horizontal to right
-    if (rowToAdd === 0 && colToAdd === 1
-        &&
-        col + colToAdd < this._board[row].length) 
-       { return false }
-    // direction horizontal to left
-    if (rowToAdd === 0 && colToAdd === -1 
-        && 
-        col + colToAdd >= 0) 
-       { return false }
-    // direction vertical up and diagonal top-left to bottom right, and top-right to bottom left
-    if (rowToAdd === 1
-        &&
-        row + rowToAdd < this._board.length) 
-       { return false }
-    // direction vertical down and diagonal bottom-left to top right, and bottom-right to top left
-    if (rowToAdd === -1
-        &&
-        row + rowToAdd >= 0) 
-       { return false }
-    return true
-  }
 
-  }
+ }
 
 
 export default Board
